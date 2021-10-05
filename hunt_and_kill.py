4e3746bffbc66e0e96a0815ff6ee2ffb0300e233
muffin <diegoxe7117@gmail.com>
2021-10-06 00:54:48 +1300

    use dicts and also print maze!

--- hunt_and_kill.py
+++ hunt_and_kill.py
@@ -15,6 +15,12 @@ DIRS = {
         'W': 1 << 3
 }
 
+O_DIRS = {
+        'N': 'S',
+        'E': 'W',
+        'S': 'N',
+        'W': 'E'
+}
 
 def init_maze(width: int, height: int) -> list[int]:
     """
@@ -31,52 +37,87 @@ def walk_maze(maze: list[int], width: int, height: int, start: tuple[int, int]) 
     # Shortcut for accessing maze
     maze_idx = lambda p: p[1] * width + p[0]
 
+    # Shortcut funcs for surrounding points
+    north   = lambda p: (p[0]   , p[1] -1)
+    east    = lambda p: (p[0] +1, p[1]   )
+    south   = lambda p: (p[0]   , p[1] +1)
+    west    = lambda p: (p[0] -1, p[1]   )
+
     def check_neighbours(pt) -> list[tuple[int, int]]:
         """
         Returns a list of possible neighbours.
         """
-        # Shortcut funcs for surrounding points
-        north   = lambda p: (p[0]   , p[1] -1)
-        east    = lambda p: (p[0] +1, p[1]   )
-        south   = lambda p: (p[0]   , p[1] +1)
-        west    = lambda p: (p[0] -1, p[1]   )
-
         # Points will be added to this list if they havent been traversed yet
-        possible_points = list()
+        possible_points = dict()
 
         # -- NORTH
         p_pt = north(pt)
         if pt[1] > 0 and maze[maze_idx(p_pt)] == 0:
-            possible_points.append(p_pt)
+            possible_points[p_pt] = "N"
 
         # -- EAST
         p_pt = east(pt)
         if pt[0] < width - 1 and maze[maze_idx(p_pt)] == 0:
-            possible_points.append(p_pt)
+            possible_points[p_pt] = "S"
 
         # -- SOUTH
         p_pt = south(pt)
         if pt[1] < height - 1 and maze[maze_idx(p_pt)] == 0:
-            possible_points.append(p_pt)
+            possible_points[p_pt] = "S"
 
         # -- WEST
         p_pt = west(pt)
         if pt[0] > 0 and maze[maze_idx(p_pt)] == 0:
-            possible_points.append(p_pt)
+            possible_points[p_pt] = "W"
 
         return possible_points
 
-    check_neighbours(start)
+    # First, connect to a random neighbour, if there is one.
+    starting_n = check_neighbours(start)
+    if starting_n:
+        n = random.choice(tuple(starting_n.keys()))
+        maze[maze_idx(n)] |= DIRS[O_DIRS[starting_n[n]]]
 
 
 def gen_maze(width: int, height: int) -> list[int]:
     maze = init_maze(width, height)
+
+    maze_idx = lambda p: p[1] * width + p[0]
     for y in range(height):
         for x in range(width):
-            if not maze[y * width + x]:
+            if not maze[maze_idx((x, y))]:
                 walk_maze(maze, width, height, (x, y))
 
     return maze
+
+def print_maze(maze: list[int], width: int, height: int) -> None:
+    """
+    Print an ASCII maze!!!! Maybe works??
+    """
+    maze_idx = lambda p: p[1] * width + p[0]
+
+    # top row
+    print(' ' + '_' * (2 * width - 1))
+
+    for y in range(height):
+        for x in range(width):
+            # left wall
+            if maze[maze_idx((x, y))] & DIRS["W"]:
+                # leave wall open if you can also go down
+                if maze[maze_idx((x, y))] & DIRS["S"]:
+                    print(' ', end='')
+                else:
+                    print('_', end='')
+
+            else:
+                print('|', end='')
+
+            if maze[maze_idx((x, y))] & DIRS["S"]:
+                print(' ', end='')
+            else:
+                print('_', end='')
+        # right wall
+        print('|')
 
 def main():
     width = height = 10
@@ -86,6 +127,7 @@ def main():
 
     print(f"Generating maze size {width}x{height}")
     maze = gen_maze(width, height)
+    print_maze(maze, width, height)
     return maze
 
 
