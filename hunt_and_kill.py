2ebd2c17d9fcce7587d1cdfc6da6f9c8f0118d7b
muffin <diegoxe7117@gmail.com>
2021-10-05 23:32:06 +1300

    initial structure

---
+++ hunt_and_kill.py
@@ -0,0 +1,27 @@
+##
+# hunt_and_kill.py
+# 05 Oct 2021
+# Generates a maze using the hunt and kill algorithm
+# S
+from sys import argv
+from enum import Enum
+import random
+
+# Cardinal directions, can be OR'd and AND'd
+DIRS = {
+        'N': 1 << 0,
+        'E': 1 << 1,
+        'S': 1 << 2,
+        'W': 1 << 3
+}
+
+def gen_maze(width: int, height: int):
+    ...
+
+
+def main():
+    ...
+
+if __name__ == "__main__":
+    print(bin(dirs.N), bin(dirs.E), bin(dirs.S), bin(dirs.W))
+    main()
